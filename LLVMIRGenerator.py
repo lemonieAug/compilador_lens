08d3d0e9c7989e888bc6ea7da8dfff37e052fa36
Augusto Gilioli <augustolg9@gmail.com>
2025-07-08 13:03:11 -0400

    .

--- LLVMIRGenerator.py
+++ LLVMIRGenerator.py
@@ -41,7 +41,7 @@ class LLVMIRGenerator:
             "; Gerado automaticamente pelo compilador",
             "",
             "target datalayout = \"e-m:w-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"",
-            "target triple = \"x86_64-pc-windows-gnu\"",
+            "target triple = \"x86_64-pc-linux-gnu\"",
             ""
         ])
     
